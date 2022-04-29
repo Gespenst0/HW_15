cbf65ea93db5ad05205ce18ae443664d12ed0ffb
Gespenst0 <101336424+Gespenst0@users.noreply.github.com>
2022-04-29 13:38:54 +0500

    Update api.py

--- api.py
+++ api.py
@@ -7,6 +7,7 @@ content_heading = ["id", "age_upon_outcome", "animal_id",
                    "date_of_birth", "outcome_subtype", "outcome_type",
                    "outcome_month", "outcome_year"]
 
+
 def get_info_from_db(id):
     result_list = []
     with sqlite3.connect("animal1.db") as connection:
@@ -16,11 +17,11 @@ def get_info_from_db(id):
                        name, ab.breed, a.color1, a.color2, date_of_birth,
                         op.outcome_subtype, op.outcome_type, od.outcome_month, od.outcome_year
                 FROM animals_task
-                INNER JOIN appearance a on a.id = animals_task.appearance_id
-                INNER JOIN animal_breeds ab on ab.id = a.breed_id
-                INNER JOIN animal_types at on a.animal_type_id = at.id
-                INNER JOIN outcome_program op on animals_task.outcome_program_id = op.id
-                INNER JOIN outcome_date od on animals_task.outcome_date_id = od.id
+                JOIN appearance a on a.id = animals_task.appearance_id
+                JOIN animal_breeds ab on ab.id = a.breed_id
+                JOIN animal_types at on a.animal_type_id = at.id
+                JOIN outcome_program op on animals_task.outcome_program_id = op.id
+                JOIN outcome_date od on animals_task.outcome_date_id = od.id
                 WHERE animals_task.id = '{id}'
                   """)
         cursor.execute(query)
@@ -37,5 +38,6 @@ def show_info(id):
     for i in range(len(a[0])):
         content = content + str(f"<p><strong>{content_heading[i]}</strong>:  {a[0][i]}</p>")
     return content
+
 
 app.run()
